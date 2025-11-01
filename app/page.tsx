090f11881b4e65f3f36af92061b64ba5cfa977c4
Tsadkan Kelemework <tsadkankelemeworktw@gmail.com>
2025-11-01 16:49:15 +0300

    nav edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Contact from "../Components/Home/Contact"
-import {Navbar} from "@/Components/Layout/Navbar"
 import { Footer } from "@/Components/Layout/Footer"
 export default function ContactPage() {
   return (
     <main className="min-h-screen">
-      <Navbar />
        <Contact />
      <Footer />
     </main>
